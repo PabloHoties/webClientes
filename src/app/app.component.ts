3f1560a5fe70c655de1031689e91596c09e1c6dd
PabloHoties <pablohotiesmarques@gmail.com>
2024-05-08 16:14:55 -0300

    feat: desenvolvimento de "navbar"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
 import { HttpClient } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConsultaClientesComponent],
+  imports: [
+    RouterOutlet, 
+    ConsultaClientesComponent, 
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
